c3eac2615c60fb340bcf1fe7e97131f0dfbe4de3
Allin_Li <allinli274@gmail.com>
2025-02-07 23:25:26 +0500

    code reorganization

---
+++ src/components/Notes.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="card bg-base-200 max-w-sm mx-auto shadow-xl">
+        <div class="card-body">
+            <h2 class="card-title justify-center mb-4">Результат</h2>
+        </div>
+    </div>
+  
+</template>
